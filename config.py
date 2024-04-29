77f1f1bcc85ecb3ea939e1fb6250376191ca34ba
gmnochez <gmnochez@outlook.com>
2024-04-29 18:36:39 -0400

    test

--- config.py
+++ config.py
@@ -109,7 +109,7 @@ def parseHadleyFile ():
 
             if iac_tool == 'terragrunt':
 
-                _path = os.path.abspath(FRAMEWORK_PATH + '/cicdtool/terragrunt')
+                _path = os.path.abspath(modPath + '/cicdtool/terragrunt')
                 if _path not in sys.path:
                     sys.path.append(_path)
 
