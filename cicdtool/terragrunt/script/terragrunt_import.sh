1dda73cccd73995ba03b74da01f75364b885f0a6
gmnochez <gmnochez@outlook.com>
2024-05-01 17:25:45 -0400

    test

--- cicdtool/terragrunt/script/terragrunt_import.sh
+++ cicdtool/terragrunt/script/terragrunt_import.sh
@@ -51,4 +51,4 @@ sed -i "s|$fullPathEnviroment|enviroment.hcl|g" $fullPathMainConfig
 sed -i "s|$fullPathGlobal|global.hcl|g" $fullPathMainConfig
 sed -i "s|key_remote_state|$deploy_path|g" $fullPathMainConfig
 
-exit $existResource
+exit 1
