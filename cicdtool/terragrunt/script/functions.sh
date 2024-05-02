57f11bc6905deae4ee722302a975bf41c34c786a
gmnochez <gmnochez@outlook.com>
2024-05-02 16:21:48 -0400

    test

--- cicdtool/terragrunt/script/functions.sh
+++ cicdtool/terragrunt/script/functions.sh
@@ -5,9 +5,9 @@ read_properties()
 {
   file=$1
   cat $file | hclq get 'locals.subscription_id'
-  subscription_id=$(cat $file | hclq get 'locals.subscription_id' | sed 's/ //g' | sed 's/""//g' | sed 's/[]//g')  
+#   subscription_id=$(cat $file | hclq get 'locals.subscription_id' | sed 's/ //g' | sed 's/""//g' | sed 's/[]//g')  
   
-  echo subscription_id $subscription_id
+#   echo subscription_id $subscription_id
   
 
 
