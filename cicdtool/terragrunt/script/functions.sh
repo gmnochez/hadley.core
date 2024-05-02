68d212e987fcf01144edf7062aa4e50d9b6e5022
gmnochez <gmnochez@outlook.com>
2024-05-02 17:02:46 -0400

    test

--- cicdtool/terragrunt/script/functions.sh
+++ cicdtool/terragrunt/script/functions.sh
@@ -8,6 +8,7 @@ read_properties()
 {
   file=$1
   subscription_id=$(cat $file | hclq get 'locals.subscription_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' ) 
+  echo $subscription_id
   if [ -n $subscription_id ] ; then export ARM_SUBSCRIPTION_ID=$subscription_id; fi
 
 }
