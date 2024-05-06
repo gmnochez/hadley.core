d977b5d403128ed4ecfbfc16cb87a19ba7111b5e
gmnochez <gmnochez@outlook.com>
2024-05-06 11:43:33 -0400

    test

--- cicdtool/terragrunt/script/terragrunt_command.sh
+++ cicdtool/terragrunt/script/terragrunt_command.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 
-
-
-
 terragrunt_validate()
 {
 
-$workingDirectory=$1
+    $workingDirectory=$1
 
-terragrunt run-all validate \
-    --terragrunt-working-dir $workingDirectory \
-    --terragrunt-include-external-dependencies \
-    --terragrunt-non-interactive
+    terragrunt run-all validate \
+        --terragrunt-working-dir $workingDirectory \
+        --terragrunt-include-external-dependencies \
+        --terragrunt-non-interactive
 
 
 }
@@ -20,12 +17,12 @@ terragrunt run-all validate \
 terragrunt_plan()
 {
 
-$workingDirectory=$1
+    $workingDirectory=$1
 
-terragrunt run-all plan \
-    --terragrunt-working-dir $workingDirectory \
-    --terragrunt-include-external-dependencies \
-    --terragrunt-non-interactive
+    terragrunt run-all plan \
+        --terragrunt-working-dir $workingDirectory \
+        --terragrunt-include-external-dependencies \
+        --terragrunt-non-interactive
 
 
 
@@ -34,34 +31,34 @@ terragrunt run-all plan \
 terragrunt_destroy()
 {
 
-$workingDirectory=$1
-$resource_declaration=$2
+    $workingDirectory=$1
+    $resource_declaration=$2
 
-existResource=0
-terragrunt --terragrunt-working-dir $workingDirectory state list
-for initialList in $(terragrunt --terragrunt-working-dir $workingDirectory state list) 
-do
-    if [[ $initialList == $resource_declaration ]]; then
-        existResource=1
-        echo Resource $initialList  exist !!
-    fi
-done
+    existResource=0
+    terragrunt --terragrunt-working-dir $workingDirectory state list
+    for initialList in $(terragrunt --terragrunt-working-dir $workingDirectory state list) 
+    do
+        if [[ $initialList == $resource_declaration ]]; then
+            existResource=1
+            echo Resource $initialList  exist !!
+        fi
+    done
 
-if $existResource == 1; then
-  
-    terragrunt run-all plan \
-        --terragrunt-working-dir $workingDirectory \
-        --terragrunt-include-external-dependencies \
-        --terragrunt-non-interactive
+    if $existResource == 1; then
     
-    terragrunt run-all destroy \
-        --terragrunt-working-dir $workingDirectory \
-        --terragrunt-ignore-external-dependencies \
-        # --terragrunt-non-interactive
+        terragrunt run-all plan \
+            --terragrunt-working-dir $workingDirectory \
+            --terragrunt-include-external-dependencies \
+            --terragrunt-non-interactive
+        
+        terragrunt run-all destroy \
+            --terragrunt-working-dir $workingDirectory \
+            --terragrunt-ignore-external-dependencies \
+            # --terragrunt-non-interactive
 
-else
-    echo "Resource doesn't exist.   Nothing to destroy !!"
-fi
+    else
+        echo "Resource doesn't exist.   Nothing to destroy !!"
+    fi
 
 
 
@@ -71,32 +68,32 @@ fi
 terragrunt_import()
 {
 
-$workingDirectory=$1
-$resource_declaration=$2
-$fullPathFileResource=$3
+    $workingDirectory=$1
+    $resource_declaration=$2
+    $fullPathFileResource=$3
 
-existResource=0
-terragrunt --terragrunt-working-dir $workingDirectory state list
-for initialList in $(terragrunt --terragrunt-working-dir $workingDirectory state list) 
-do
+    existResource=0
+    terragrunt --terragrunt-working-dir $workingDirectory state list
+    for initialList in $(terragrunt --terragrunt-working-dir $workingDirectory state list) 
+    do
 
-    if [[ $initialList == $resource_declaration ]]; then
+        if [[ $initialList == $resource_declaration ]]; then
+            existResource=1
+            # echo $initialList state already exist !!
+
+        fi
+    done
+
+    if [[ $existResource == 0 ]]; then   
+        resource_id=$(cat $fullPathFileResource | hclq get 'locals.resource_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
+        terragrunt import \
+            --terragrunt-working-dir $workingDirectory \
+            --terragrunt-include-external-dependencies \
+            --terragrunt-non-interactive $resource_declaration $resource_id
+            
         existResource=1
-        # echo $initialList state already exist !!
 
     fi
-done
-
-if [[ $existResource == 0 ]]; then   
-    resource_id=$(cat $fullPathFileResource | hclq get 'locals.resource_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-    terragrunt import \
-        --terragrunt-working-dir $workingDirectory \
-        --terragrunt-include-external-dependencies \
-        --terragrunt-non-interactive $resource_declaration $resource_id
-        
-    existResource=1
-
-fi
 
 
 
@@ -106,12 +103,12 @@ fi
 terragrunt_apply()
 {
 
-$workingDirectory=$1
+    $workingDirectory=$1
 
-terragrunt run-all apply \
-    --terragrunt-working-dir $workingDirectory \
-    --terragrunt-include-external-dependencies \
-    --terragrunt-non-interactive
+    terragrunt run-all apply \
+        --terragrunt-working-dir $workingDirectory \
+        --terragrunt-include-external-dependencies \
+        --terragrunt-non-interactive
 
 }
 
