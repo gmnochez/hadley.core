7d544291720a4e43511976a31eb9e74c269879e6
gmnochez <gmnochez@outlook.com>
2024-05-16 18:02:23 -0400

    test

--- config.py
+++ config.py
@@ -98,17 +98,18 @@ def parseHadleyFile ():
                 print("Modulo not found in the configuration of the project " + project['name'])
                 exit()
 
-
-            if arrayFrameworks.count(modPath) == 0:            
-                for modules in project['modules']:
-                    modPath = CICD_ROOT_PATH + '/' + FRAMEWORK_PATH + '/' + modules['name']
+                       
+            for modules in project['modules']:
+                print('Downloading repository modules ...')
+                modPath = CICD_ROOT_PATH + '/' + FRAMEWORK_PATH + '/' + modules['name']
+                                   
+                if arrayFrameworks.count(modPath) == 0: 
                     if os.path.isdir(modPath): 
                         os.system('rm -rf ' + modPath)
-                    
-                    print('Downloading repository modules ...')
                     command = 'git clone ' + modules['repository'] + ' ' + CICD_ROOT_PATH + '/' + FRAMEWORK_PATH + '/' + modules['name']
                     os.system(command)
                     arrayFrameworks.append(modPath)
+                    break
 
             if os.path.isfile(CICD_ROOT_PATH + '/' + main_config):
                 isok_main_config = True
