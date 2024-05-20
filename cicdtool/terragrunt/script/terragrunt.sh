a011ee3718ce6fa99c126ed6e8070619e35101fe
gmnochez <gmnochez@outlook.com>
2024-05-20 19:17:12 -0400

    test

--- cicdtool/terragrunt/script/terragrunt.sh
+++ cicdtool/terragrunt/script/terragrunt.sh
@@ -29,10 +29,10 @@ fullPathGlobal="$CICD_ROOT_PATH/$global_definition"
  
 # file_name=$(echo $file_resource |  sed 's/\.hcl//g')
 file_name=$(echo $file_resource |  sed 's#.*/##')
-echo file_name $file_name
+# echo file_name $file_name
 
 fullPathFileResource="$workingDirectory/$file_name/$file_resource.hcl"
-echo fullPathFileResource $fullPathFileResource
+# echo fullPathFileResource $fullPathFileResource
 
 export deployDirectory="$workingDirectory/$file_name"
 # cp -f "$workingDirectory/terragrunt.hcl" "$workingDirectory/$file_name/terragrunt.hcl"
