cd4d9dd0f8da700605b358bd3ebb088c90705c78
gmnochez <gmnochez@outlook.com>
2024-05-20 19:54:18 -0400

    test

--- cicdtool/terragrunt/terragrunt.py
+++ cicdtool/terragrunt/terragrunt.py
@@ -17,8 +17,8 @@ def terragruntCommand(resource_action, deploy_action, CICD_ROOT_PATH, FRAMEWORK_
 
 def checkResourceDefinition(CICD_ROOT_PATH, deploy_path, file_resource):
     # file_name = os.path.splitext(file_resource)[0]
-    str_index = str(file_resource).split('/').count()
-    file_name = str(file_resource).split('/')[str_index]
+    str_index = str(file_resource).split('/')
+    file_name = str(file_resource).split('/')[len(str_index)]
     
     print (file_resource)
     print (file_name)
