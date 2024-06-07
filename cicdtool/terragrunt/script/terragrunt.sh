7acebd4f7a63f865ce72395053e6a5b2473211ec
gmnochez <gmnochez@outlook.com>
2024-06-07 17:22:17 -0400

    test

--- cicdtool/terragrunt/script/terragrunt.sh
+++ cicdtool/terragrunt/script/terragrunt.sh
@@ -15,7 +15,7 @@ global_definition=${12}
 frameworkFullPath=${13}
 
 
-ls $frameworkFullPath/script/functions.sh
+ls -lat $frameworkFullPath/script/functions.sh
 sed -i 's/\r//g' $frameworkFullPath/script/functions.sh
 sed -i 's/\r//g' $frameworkFullPath/script/terragrunt_command.sh  
 source $frameworkFullPath/script/functions.sh
