b39bc8e83f042bbe761bb81215d50c1c2b495c59
gmnochez <gmnochez@outlook.com>
2024-06-07 16:48:44 -0400

    test

--- cicdtool/terragrunt/script/functions.sh
+++ cicdtool/terragrunt/script/functions.sh
@@ -1,4 +1,4 @@
-/bin/bash
+#!/bin/bash
 
 
 
@@ -11,27 +11,27 @@ read_properties()
 # AZURE 
 
   subscription_id=$(cat $file | hclq get 'locals.subscription_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-  if [[ $subscription_id != "" ]] ; then export ARM_SUBSCRIPTION_ID=$subscription_id; fi
+  if [ $subscription_id != "" ] ; then export ARM_SUBSCRIPTION_ID=$subscription_id; fi
 
   client_id=$(cat $file | hclq get 'locals.client_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-  if [[ $client_id != "" ]] ; then export ARM_CLIENT_ID=$client_id; fi
+  if [ $client_id != "" ] ; then export ARM_CLIENT_ID=$client_id; fi
 
   client_secret=$(cat $file | hclq get 'locals.client_secret' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-  if [[ $client_secret != "" ]] ; then export ARM_CLIENT_SECRET=$client_secret; fi
+  if [ $client_secret != "" ] ; then export ARM_CLIENT_SECRET=$client_secret; fi
 
   tenant_id=$(cat $file | hclq get 'locals.tenant_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-  if [[ $tenant_id != "" ]] ; then export ARM_TENANT_ID=$tenant_id; fi
+  if [ $tenant_id != "" ] ; then export ARM_TENANT_ID=$tenant_id; fi
 
 # AWS
 
   aws_account_id=$(cat $file | hclq get 'locals.aws_account_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-  if [[ $aws_account_id != "" ]] ; then export AWS_ACCOUNT_ID=$aws_account_id; fi
+  if [ $aws_account_id != "" ] ; then export AWS_ACCOUNT_ID=$aws_account_id; fi
   
   aws_access_key_id=$(cat $file | hclq get 'locals.aws_access_key_id' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-  if [[ $aws_access_key_id != "" ]] ; then export AWS_ACCESS_KEY_ID=$aws_access_key_id; fi
+  if [ $aws_access_key_id != "" ] ; then export AWS_ACCESS_KEY_ID=$aws_access_key_id; fi
   
   aws_secret_access_key=$(cat $file | hclq get 'locals.aws_secret_access_key' | sed 's/ //g' | sed 's/\"\"//g' | sed 's/\[\]//g' | sed 's/^"\(.*\)"$/\1/' ) 
-  if [[ $aws_secret_access_key != "" ]] ; then export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key; fi
+  if [ $aws_secret_access_key != "" ] ; then export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key; fi
   
 
 
