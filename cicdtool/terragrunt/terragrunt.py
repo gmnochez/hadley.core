eac45637e89d257355f07843aa1ad957703833c4
gmnochez <gmnochez@outlook.com>
2024-06-19 10:15:45 -0400

    test

--- cicdtool/terragrunt/terragrunt.py
+++ cicdtool/terragrunt/terragrunt.py
@@ -57,7 +57,6 @@ def cicdTerragrunt (CICD_ROOT_PATH, FRAMEWORK_PATH, frameworkFullPath, module_fr
             if resource_definition_deploy != 'true':
                 continue
 
-            print("cicdTerragrunt")
 
             existCheckFile = checkResourceDefinition(CICD_ROOT_PATH, deploy_path, file_resource)
 
