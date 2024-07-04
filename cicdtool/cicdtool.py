c1b89a587eed7e49140ba41f231d93c398d6be0a
gmnochez <gmnochez@outlook.com>
2024-07-04 12:44:34 -0400

    test

--- cicdtool/cicdtool.py
+++ cicdtool/cicdtool.py
@@ -9,9 +9,9 @@ import subprocess
 def toolCommand(resource_action, deploy_action, CICD_ROOT_PATH, FRAMEWORK_PATH, frameworkFullPath, module_framework, main_config, resource_type, resource_api, deploy_path, file_resource, enviroment_definition, global_definition, iac_tool):
     scriptTool = ''
     if iac_tool == 'terragrunt':
-        scriptTool = '/script/terragrunt.sh'
+        scriptTool = '/cicdtool/script/terragrunt.sh'
     if iac_tool == 'bicep':
-        scriptTool = '/script/bicep.sh'
+        scriptTool = '/cicdtool/script/bicep.sh'
         
     
     execScript = "sh " + frameworkFullPath + scriptTool + ' ' + resource_action + ' ' + deploy_action + ' ' + CICD_ROOT_PATH + ' ' + FRAMEWORK_PATH + ' ' + module_framework + ' ' + main_config + ' ' + resource_type + ' ' + resource_api + ' ' + deploy_path + ' ' + file_resource + ' ' + enviroment_definition + ' ' + global_definition + ' ' + frameworkFullPath
