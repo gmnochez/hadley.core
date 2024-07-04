d93419d75494ca5afa1eb2d1965eef9285f76f0f
gmnochez <gmnochez@outlook.com>
2024-07-04 16:53:24 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -69,6 +69,7 @@ cp $sourceMainBicep/main.bicep "$sourceBicepDeploy/main_$file_name.bicep"
 cp $deployDirectory/$file_name.bicep "$sourceBicepDeploy/param_$file_name.bicep"
 
 sed -i "s|param hadley_definition_param|params|g" "$sourceBicepDeploy/param_$file_name.bicep"
+cat $sourceBicepDeploy/param_$file_name.bicep
 extractedParameters=$(cat $sourceBicepDeploy/param_$file_name.bicep)
 
 sed -i "s|hadley_resource|$file_name|g" "$sourceBicepDeploy/main_$file_name.bicep"
