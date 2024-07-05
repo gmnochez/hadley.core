8c7cad0e6f47b7e62c20eaa0ac88c3694c5862e1
gmnochez <gmnochez@outlook.com>
2024-07-05 12:26:50 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -74,7 +74,7 @@ echo "$extractedParameters"
 
 sed -i "s|hadley_resource|$file_name|g" "$sourceBicepDeploy/main_$file_name.bicep"
 sed -i "s|hadley_source_bicep|$fileNameImplementation|g" "$sourceBicepDeploy/main_$file_name.bicep"
-sed -i "s|hadley_params|"$extractedParameters"|g" "$sourceBicepDeploy/main_$file_name.bicep"
+sed -i "s|hadley_params|\"$extractedParameters\"|g" "$sourceBicepDeploy/main_$file_name.bicep"
 
 cat "$sourceBicepDeploy/main_$file_name.bicep"
 
