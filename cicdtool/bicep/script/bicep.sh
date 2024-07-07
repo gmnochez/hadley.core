3a712950cc4ea95e7f0e480fcb7d00b9497abfe2
gmnochez <gmnochez@outlook.com>
2024-07-07 18:59:38 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -86,7 +86,7 @@ extractedParameters=${extractedParameters%?}
 
 # tags=$(grep -oP '(?<=tags*:*{).*?(?=})' <<< "$extractedParameters")
 
-grep -oP 'tags\K.*?(?=})' <<< "$extractedParameters"
+grep -oP 'tags(?:\s[^>]*)?>\K.*?(?=})' <<< "$extractedParameters"
 
 
 
