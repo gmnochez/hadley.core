b9ed80022f210e64f45e71b9cd9741c1815d370b
gmnochez <gmnochez@outlook.com>
2024-07-08 15:17:40 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -103,7 +103,7 @@ do
     if [[ $line != *"["* ]]  &&  [[ $line != *"]"* ]] ; then 
         key=$(echo $line |awk -F '=' '{print $1}')
         value=$(echo $line |awk -F '=' '{print $2}')
-        sed -i "s|$line|{\n key = '"$key"'\n value = "$value" \n },\n|g"  "./temp.txt"     
+        sed -i "s|$line|{\n key = "$key"\n value = "$value" \n },\n|g"  "./temp.txt"     
     fi
   
 done
