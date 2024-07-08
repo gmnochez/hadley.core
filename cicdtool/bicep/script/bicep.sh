4ad179a2202a622edb79e47dfa272eaa9412f2e1
gmnochez <gmnochez@outlook.com>
2024-07-08 16:19:17 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -109,8 +109,8 @@ do
         sed -i "s|$line|$newLine|g"  "./temp.txt"     
     fi
     if [[ $line == *"]"* ]] ; then 
-        line1="},"
-        line2="}"
+        line1="},\n^]"
+        line2="}\n]"
         echo "$line"
         sed -i "s|$line1|$line2|g"  "./temp.txt"
     fi
