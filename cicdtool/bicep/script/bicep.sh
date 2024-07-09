59db7728c919a5f0d5b0e2644f1ce52adfc600c0
gmnochez <gmnochez@outlook.com>
2024-07-09 11:30:17 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -110,7 +110,7 @@ do
     fi
     if [[ $line == *"]"* ]] ; then 
         
-        line1=$(cat "./temp.txt" | sed -n '/}/\n/{/p')
+        line1=$(cat "./temp.txt" | sed -n '/}/,/{/g')
         echo "$line1"
         line2="},\n{"
         echo "$line"
