2d159bb804a823573024ae4fba564179bfe4d19a
gmnochez <gmnochez@outlook.com>
2024-07-10 18:24:16 -0400

    test

--- cicdtool/bicep/script/functions.sh
+++ cicdtool/bicep/script/functions.sh
@@ -69,6 +69,8 @@ transformFileBicepToHcl()
     tags=$(cat "$fileBicep" | sed -n "/$arrayProperty/,/}/p")
     tags2=$(cat "$fileHcl" | sed -n "/$arrayProperty/,/}/p")
     tags2=$(echo "$tags2" | sed "s|\n|\\n|g")
+    tags2=$(echo "$tags2" | sed "s|}|\}|g")
+    tags2=$(echo "$tags2" | sed "s|{|\{|g")
     echo "$tags2"
     sed -i ':a;N;$!ba;s|'"$tags2"'|tags_param|g' "$fileHcl"
     
