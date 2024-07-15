7db18aab117703e253bec5e75081dcbdc65deec1
gmnochez <gmnochez@outlook.com>
2024-07-15 12:00:02 -0400

    test

--- cicdtool/bicep/script/functions.sh
+++ cicdtool/bicep/script/functions.sh
@@ -149,13 +149,11 @@ transformPropertyHclToBicep()
     fi 
 
 
-    # sed -i "s/\(.*\)}/hadley_property\n}/g" "$fileBicep"
+    sed -i "s/\(.*\)}/hadley_property\n}/g" "$fileBicep"
    
-    # tags=$(echo "$tags" | sed "s|[|{\n|g")
-    # tags=$(echo "$tags" | sed "s|]|\n}|g")
-    # tags=$(echo "$tags" | sed "s|]|\n}|g")
-    # tags=$(echo "$tags" | sed "s|{|\n{|g")
-    # tags=$(echo "$tags" | sed -r '/^\s*$/d')
+    tags=$(echo "$tags" | sed "s|[|{\n|g")
+    tags=$(echo "$tags" | sed "s|]|}\n}|g")
+    tags=$(echo "$tags" | sed -r '/^\s*$/d')
 
 
     echo "$tags" > temp.txt
