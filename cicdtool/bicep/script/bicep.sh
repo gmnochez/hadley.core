bc420d3432c816dcb5e9eb1f52d6b9e5b7b6e217
gmnochez <gmnochez@outlook.com>
2024-07-15 19:06:42 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -117,8 +117,6 @@ if [[ $existProperty == "true" ]];then
     echo "Parameters imported from: $sourceParameters"
 fi
 
-cat "$sourceBicepDeploy/param_$file_name.bicep"
-
 
 az login \
     --only-show-errors \
