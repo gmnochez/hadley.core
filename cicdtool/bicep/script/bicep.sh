4445720cf80b4f2721f9cce78f01258580a9b05f
gmnochez <gmnochez@outlook.com>
2024-07-15 21:27:08 -0400

    test

--- cicdtool/bicep/script/bicep.sh
+++ cicdtool/bicep/script/bicep.sh
@@ -129,7 +129,7 @@ az login \
 
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
-
+cat "$sourceBicepDeploy/deploy_$file_name.bicep"
 if [[ $deploy_action == "create" ]];then 
 
     if [[ $resource_action == "plan" ]];then
